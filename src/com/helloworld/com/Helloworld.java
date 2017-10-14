c662807c051b7b4c214c35a0af88f710f51321f4
elhadjimomarndiaye <elmomar86@gmail.com>
2017-10-14 18:15:36 +0000

    initialisation de mon premier projet

---
+++ src/com/helloworld/com/Helloworld.java
@@ -0,0 +1,9 @@
+package com.helloworld.com;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Mon premier code Java");
+	}
+}
